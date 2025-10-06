87d1037af5c45ca9fdf90b8e76df1e9a1096aa1b
张先生 <2959917355@qq.com>
2025-10-07 00:06:56 +0800

    desc: 增加热门推荐模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -12,17 +12,22 @@
     <view class="category">
       <CategoryPanel :list="categoryList" />
     </view>
+    <!-- 热门推荐 -->
+    <view class="hot">
+      <HotPanel :list="hotList" />
+    </view>
   </view>
 </template>
 
 <script lang="ts" setup>
-import type { BannerItem, CategoryItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 
 import CustomNavbar from '@/components/CustomNavbar.vue'
-import { getHomeBannerApi, getHomeCategoryApi } from '@/services/home'
+import { getHomeBannerApi, getHomeCategoryApi, getHomeHotApi } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import CategoryPanel from '@/components/CategoryPanel.vue'
+import HotPanel from '@/components/HotPanel.vue'
 
 //获取轮播图数据
 const bannerList = ref<BannerItem[]>([]) // 轮播图数据
@@ -39,10 +44,20 @@ const getCategoryData = async () => {
   console.log('前台分类数据', res)
   categoryList.value = res.result
 }
+
+//获取热门推荐数据
+const hotList = ref<HotItem[]>([]) // 热门推荐数据
+const getHotData = async () => {
+  const res = await getHomeHotApi()
+  console.log('热门推荐数据', res)
+  hotList.value = res.result
+}
+
 //页面加载
 onLoad(() => {
   getHomeBannerData()
   getCategoryData()
+  getHotData()
 })
 </script>
 
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,4 +1,4 @@
-import type { BannerItem, CategoryItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { http } from '@/utils/http'
 
 /**
@@ -25,5 +25,17 @@ export const getHomeCategoryApi = () => {
   return http<CategoryItem[]>({
     method: 'GET',
     url: '/home/category/mutli',
+  })
+}
+
+/**
+ * 获取首页热门推荐数据的API函数
+ * 该函数用于从后端获取首页的热门推荐商品/内容数据
+ * @returns 返回一个Promise，解析为热门推荐数据数组
+ */
+export const getHomeHotApi = () => {
+  return http<HotItem[]>({
+    method: 'GET',
+    url: '/home/hot/mutli',
   })
 }
